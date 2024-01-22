695a1090cae4dfd5e08136c8e6b6e85cfb23edf2
Clawiatura <maksimov19992805@icloud.com>
2024-01-22 14:00:40 +0200

    Dos'e

---
+++ Dos'e/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Как вас зовут? ");
+string name = Console.ReadLine();
+
+Console.Write("Ваша фамилия? ");
+string surname = Console.ReadLine();
+
+Console.Write("Ваш возраст? ");
+int age = int.Parse(Console.ReadLine());
+
+Console.Write("Вы работаете? ");
+string job = Console.ReadLine();
+
+Console.Write("Вы женаты? ");
+string wife = Console.ReadLine();
+
+string finish = $"\t\t\tДосье\n\n\nИмя\tФамилия\t\tВозраст\tРабота\tЖенаты\n{name}\t{surname}\t{age}\t{job}\t{wife}\a";
+
+Console.WriteLine(finish);
+
+
