a707ce14b44ec41d33299a2da9b8829595d9ac4a
Clawiatura <maksimov19992805@icloud.com>
2024-01-22 13:48:32 +0200

    tema 7 dz 1

---
+++ Vvod i vuvod dannux/Program.cs
@@ -0,0 +1,9 @@
+﻿const  double P =  Math.PI;
+Console.WriteLine("Введите радиус окружности");
+float r = float.Parse(Console.ReadLine());
+Console.WriteLine("Введите формулу радиуса окружности и производящую просчет площади круга");
+string formuls = Console.ReadLine();
+
+string finish = $"S={P*(r*r):F2}";
+Console.WriteLine(finish);
+
